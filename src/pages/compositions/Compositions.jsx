1037dcd1a5c6f28580f5883e8d5ac94215033e91
shk9t <147887719+shk9t@users.noreply.github.com>
2024-04-24 19:09:32 +0300

    new file:   src/Componetns/VoiceRecorder/VoiceRecorder.jsx 	new file:   src/Componetns/VoiceRecorder/VoiceRecorder.module.css 	modified:   src/pages/compositions/Compositions.jsx

--- src/pages/compositions/Compositions.jsx
+++ src/pages/compositions/Compositions.jsx
@@ -1,11 +1,13 @@
 import SideMenu from "../../Componetns/sideMenu/SideMenu.jsx";
 import Player from "../../Componetns/player/Player.jsx";
 import styles from "./compositions.module.css"
+import Voicerecorder from "../../Componetns/VoiceRecorder/VoiceRecorder.jsx";
 
 function Compositions(props) {
   return (
     <main className={styles.compositions}>
       <SideMenu activeSection={'compositions'}/>
+      <Voicerecorder/>
       <Player />
     </main>
   );
