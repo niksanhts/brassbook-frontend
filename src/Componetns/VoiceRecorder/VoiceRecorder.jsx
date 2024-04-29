8d796466c5e9ea80138a1eef23abc9dea9e43880
shk9t <147887719+shk9t@users.noreply.github.com>
2024-04-29 16:36:33 +0300

    modified:   src/Componetns/VoiceRecorder/VoiceRecorder.jsx

--- src/Componetns/VoiceRecorder/VoiceRecorder.jsx
+++ src/Componetns/VoiceRecorder/VoiceRecorder.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
 import YellowButton from "../yellowButton/YellowButton";
 import classes from './VoiceRecorder.module.css';
 
 function Voicerecorder() {
+        const [recording, setRecording] = useState(false);
+        const [mediaRecorder, setMediaRecorder] = useState(null);
+      
+        const startRecording = () => {
+          navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+              const recorder = new MediaRecorder(stream);
+              recorder.start();
+              setRecording(true);
+              setMediaRecorder(recorder);
+            })
+            .catch(error => {
+              console.error('Error accessing microphone:', error);
+            });
+      
+        const stopRecording = () => {
+          if (mediaRecorder) {
+            mediaRecorder.stop();
+            setRecording(false);
+          }
+        };
+}
   
         return (
           <div className={classes.voicerecorder}>
@@ -12,8 +34,11 @@ function Voicerecorder() {
                 <div className={classes.voicerecorder_subtitle}>
                     Ты можешь записать свою игру на инструменте и, если захочешь, она будет проанализирована на ошибки.
                 </div>
-                <YellowButton to={'/'} className={'voicerecorder_btn button-type-2'}>ЗАПИСАТЬ ПРОИЗВЕДЕНИЕ</YellowButton>
-               
+                {/* <YellowButton onClick={startRecording} className={'voicerecorder_btn button-type-2'}>ЗАПИСАТЬ ПРОИЗВЕДЕНИЕ</YellowButton> */}
+                <div>
+                        {/* <button onClick={startRecording}>Start Recording</button>
+                        <button onClick={stopRecording}>Stop Recording</button> */}
+                </div>
             </div>
   
 
